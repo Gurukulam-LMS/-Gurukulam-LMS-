8078c2fb3dcea1d8c37bc602ad7e1702ebd4ea71
fubuk-i <sanky.dhone@gmail.com>
2021-09-09 12:27:23 +0530

    Payment Gateway

--- controllers/payment.js
+++ controllers/payment.js
@@ -24,6 +24,41 @@ exports.orders = async (req, res) => {
     res.status(200).send(order);
 } catch (error) {
     res.status(500).send(error);
-}
-  
+} 
+};
+
+exports.success = async (req, res) => {
+    try {
+        // getting the details back from our font-end
+        const {
+            orderCreationId,
+            razorpayPaymentId,
+            razorpayOrderId,
+            razorpaySignature,
+        } = req.body;
+
+        // Creating our own digest
+        // The format should be like this:
+        // digest = hmac_sha256(orderCreationId + "|" + razorpayPaymentId, secret);
+        const shasum = crypto.createHmac("sha256", process.env.RAZORPAY_SECRET);
+
+        shasum.update(`${orderCreationId}|${razorpayPaymentId}`);
+
+        const digest = shasum.digest("hex");
+
+        // comaparing our digest with the actual signature
+        if (digest !== razorpaySignature)
+            return res.status(400).json({ msg: "Transaction not legit!" });
+
+        // THE PAYMENT IS LEGIT & VERIFIED
+        // YOU CAN SAVE THE DETAILS IN YOUR DATABASE IF YOU WANT
+
+        res.json({
+            msg: "success",
+            orderId: razorpayOrderId,
+            paymentId: razorpayPaymentId,
+        });
+    } catch (error) {
+        res.status(500).send(error);
+    }
 };
