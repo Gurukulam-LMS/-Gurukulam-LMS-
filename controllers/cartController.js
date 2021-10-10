81b24bcadcc8da885e92ab9e7640f022e93440e5
Modassir Ali <alimodassir@gmail.com>
2021-10-11 00:56:12 +0530

    fixed cart bug

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -63,13 +63,27 @@ module.exports.modifyCart = async (req, res) => {
 module.exports.getCart = async (req, res) => {
   try {
     const { userId } = req.body;
+
     if (!userId) return res.status(404).json({ message: "userId not found" });
     const cartDetails = await Cart.findOne({ userId });
     if (!cartDetails)
       return res.json({ message: "User no found", ok: false, cart: [] });
+
     return res.json({ cart: cartDetails.coursesId, ok: true });
   } catch (error) {
     console.log(error);
     return res.json({ message: "Cart details failed", ok: false });
+  }
+};
+
+module.exports.clearCart = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const deleteCart = await Cart.findOneAndDelete({ userId });
+    if (!deleteCart) return res.status(404).json({ message: "Cart missing" });
+    return res.status(200).json({ message: "Cleared Cart" });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: "Unable to clear cart", err });
   }
 };
--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -5,5 +5,6 @@ const cartController = require("../controllers/cartController");
 
 router.get("/modifyCart/:query", cartController.modifyCart);
 router.post("/getCart", cartController.getCart);
+router.get("/clearCart/:userId", cartController.clearCart);
 
 module.exports = router;
